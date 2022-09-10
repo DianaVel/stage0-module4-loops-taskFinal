723154a4b8577582311dcb9c249dac3f8f4bd79d
dveltauri <diana.veltauri1@gmail.com>
2022-09-10 03:40:08 -0700

    Init commit

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,25 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        String res="";
+        for(int i=1; i<=cathetusLength; i++){
+            for (int j=cathetusLength; j>1; j--){
+                if(j>i){
+                    System.out.print(" ");
+                }else {
+                    System.out.print(j);
+                }
+           }
+            for (int j=1; j<=cathetusLength; j++) {
+                if (j <= i) {
+                    System.out.print(j);
+                }
+            }
+           System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(9);
     }
 }
